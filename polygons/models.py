f8894c4d2e75fbc77d4fa9c7ebc5e8be2d187ae0
Denis Sleptsov <hurrdurrrderp@gmail.com>
2021-10-15 21:33:43 +0300

    GisPolygon model

--- polygons/models.py
+++ polygons/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import os
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, JSON, Integer, String
+from geoalchemy2 import Geometry
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-# Create your models here.
+
+engine = create_engine(
+    f'postgresql://postgres:{os.environ["POSTGRES_PASS"]}@localhost:5432/postgis_31_sample')
+Session = sessionmaker(engine)
+Base = declarative_base()
+
+
+class GisPolygon(Base):
+    __tablename__ = 'gis_polygon'
+    _created = Column(DateTime(timezone=False))
+    _updated = Column(DateTime(timezone=False))
+    id = Column(Integer, primary_key=True)
+    class_id = Column(Integer)
+    name = Column(String)
+    props = Column(JSON)
+    geom = Column(Geometry('POLYGON'))
+
+    def __repr__(self):
+        return self.name
--- polygons/views.py
+++ polygons/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from .models import Session, GisPolygon
 
 
 def index(request):
+    with Session() as session:
+        result = session.query(GisPolygon).all()
+        for row in result:
+            print(row)
     return HttpResponse("List of all polygons")
